e460c3a6add7c6a895c731a19f407412a0774677
huju8593 <526097928@qq.com>
2018-10-18 11:29:19 +0800

    删除注解

--- Project/src/main/java/cn/huatek/spring/controller/UserController.java
+++ Project/src/main/java/cn/huatek/spring/controller/UserController.java
@@ -26,17 +26,14 @@ public class UserController {
 
 	@RequestMapping(value = "/list_user.do")
 	public String showListUser(ModelMap modelMap) {
-		// ????????งา?
 		List<User> users = userService.finAllUser();
 		List<Project> projects = userService.finAllProject();
 		List<Time> times = userService.finAllTime();
 		List<Map<String, Object>> maps = userService.findPersonHour();
-		// ???????,??????
 		modelMap.addAttribute("users", users);
 		modelMap.addAttribute("projects", projects);
 		modelMap.addAttribute("times", times);
 		modelMap.addAttribute("maps", maps);
-		// ??????
 		return "../list2";
 	}
 
